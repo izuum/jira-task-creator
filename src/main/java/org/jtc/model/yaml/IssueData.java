b32a43d1a6a244879d2765bcfd022d0a08ba42f5
Roman <dstep.ds@gmail.com>
2026-03-06 13:56:20 +0300

    QL-21 добавил параметры для корректной сериализации и десериализации полей

--- src/main/java/org/jtc/model/yaml/IssueData.java
+++ src/main/java/org/jtc/model/yaml/IssueData.java
@@ -1,11 +1,13 @@
 package org.jtc.model.yaml;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class IssueData {
     private String project;
     private String summary;
+    @JsonProperty("type")
     private String issueType;
     private String priority;
     private String description;
